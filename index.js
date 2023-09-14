d5843b64692d7b35087f6a3e3a97845112db07d6
saannsaar <sannsaarinen@gmail.com>
2023-09-14 09:47:26 +0300

    13.4

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Sequelize, Model, QueryTypes, DataTypes } = require('sequelize')
 
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
@@ -12,17 +13,18 @@ Blog.init({
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
+        unique: true
     },
     author: {
-        type: DataTypes.TEXT,
+        type: DataTypes.STRING,
     },
     url: {
-        type: DataTypes.TEXT,
+        type: DataTypes.STRING,
         allowNull: false
     },
     title: {
-        type: DataTypes.TEXT,
+        type: DataTypes.STRING,
         allowNull: false
     },
     likes: {
@@ -36,17 +38,22 @@ Blog.init({
     timestamps: false,
     modelName: 'blog'
 })
+Blog.sync()
 
 app.get('/api/blogs', async(req, res) => {
     const blogs = await Blog.findAll()
-    console.log('Executing (default): SELECT * FROM blogs')
-    blogs.forEach((blog) => {
-        console.log(`${blog.author}: ${blog.title}, ${blog.likes} likes`)
-    })
+    // console.log('Executing (default): SELECT * FROM blogs')
+    // blogs.forEach((blog) => {
+    //     console.log(`${blog.author}: ${blog.title}, ${blog.likes} likes`)
+    // })
+
+    console.log(JSON.stringify(blogs))
+
     res.json(blogs)
 })
 
 app.post('/api/blogs', async (req, res) => {
+   console.log(req.body)
    try {
     console.log(req.body)
     const blog = await Blog.create(req.body)
@@ -56,6 +63,20 @@ app.post('/api/blogs', async (req, res) => {
    }
   })
 
+  app.get('/api/blogs/:id', async (req, res) => {
+    const blog = await Blog.findByPk(req.params.id)
+    if (blog) {
+        res.json(blog)
+    } else {
+        res.status(404).end()
+    }
+  })
+ 
+app.delete('/api/blogs/:id', async(req, res) => {
+    const blog = await Blog.findByPk(req.params.id)
+    await blog.destroy()
+    res.json(blog)
+})
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
