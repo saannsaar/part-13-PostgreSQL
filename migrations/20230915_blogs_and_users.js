22ff5f96d5fe5ef4d4a3faf6fbdceda2eb0bc77e
saannsaar <sannsaarinen@gmail.com>
2023-09-15 10:16:30 +0300

    13.17

---
+++ migrations/20230915_blogs_and_users.js
@@ -0,0 +1,81 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable('blogs', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            unique: true
+        },
+        author: {
+            type: DataTypes.STRING,
+        },
+        url: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        likes: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+          },
+          updated_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+          },
+    })
+    await queryInterface.createTable('users', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            unique: true
+          },
+          username: {
+            type: DataTypes.STRING,
+            unique: true,
+            validate: {
+                isEmail: {
+                    msg: 'Invalid email address'
+                }
+            },
+            allowNull: false
+          },
+          name: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          created_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+          },
+          updated_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+          },
+    })
+    
+    await queryInterface.addColumn('blogs', 'user_id', {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'users', key: 'id' },
+    })
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable('blogs')
+    await queryInterface.dropTable('users')
+  },
+}
--- util/db.js
+++ util/db.js
@@ -2,10 +2,29 @@ const Sequelize = require('sequelize')
 const { DATABASE_URL } = require('./config')
 const logger = require('./logger')
 const sequelize = new Sequelize(DATABASE_URL)
+const { Umzug, SequelizeStorage } = require('umzug')
+
+
+const runMigrations = async () => {
+    const migrator = new Umzug({
+      migrations: {
+        glob: 'migrations/*.js',
+      },
+      storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+      context: sequelize.getQueryInterface(),
+      logger: console,
+    })
+    
+    const migrations = await migrator.up()
+    console.log('Migrations up to date', {
+      files: migrations.map((mig) => mig.name),
+    })
+  }
 
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
+    await runMigrations()
     logger.info('Connected to the database succesfully!')
   } catch (err) {
     logger.error('Failed to connect to the database!')
